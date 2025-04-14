c86be246b1e6e99aae013d9764267d168f924ba9
Torezhan Yelamanov <yyelamanooovv@gmail.com>
2025-04-14 22:24:05 +0500

    delete some files, cause i forgot that we have 2 parts................

--- logger/Logger.java
+++
@@ -1,8 +0,0 @@
-package logger;
-
-
-public class Logger {
-    public static void log(String handler, String message) {
-        System.out.println("[Logger] " + handler + ": " + message);
-    }
-}
