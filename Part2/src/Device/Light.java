d975e75b4bac7f77208c3d2b4803142b44b32b58
Torezhan Yelamanov <yyelamanooovv@gmail.com>
2025-04-15 18:55:23 +0500

    Created Light class to illuminate the way for the command pattern

--- Part2/src/Device/Light.java
+++ Part2/src/Device/Light.java
@@ -1,12 +1,20 @@
 package Device;
 
 public class Light {
+    private boolean isOn = false; // Изначально свет выключен
+
     public void turnOn() {
+        isOn = true;
         System.out.println("[Light] Turning ON");
     }
 
     public void turnOff() {
+        isOn = false;
         System.out.println("[Light] Turning OFF");
+    }
+
+    public boolean isOn() {
+        return isOn;
     }
 }
 
