6d8ea50dec96c6487340fbb882480a9cae967977
Torezhan Yelamanov <yyelamanooovv@gmail.com>
2025-04-15 18:54:46 +0500

    Wrote the client code to bring the smart home to life with undo/redo

--- Part2/src/Main/Main.java
+++ Part2/src/Main/Main.java
@@ -1,25 +1,41 @@
 package Main;
 
+import Command.*;
+import Device.*;
+import Remote.*;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Light light = new Light();
         Thermostat thermostat = new Thermostat();
+        Door door = new Door();
 
         Command turnOnLight = new TurnOnLightCommand(light);
+        Command turnOffLight = new TurnOffLightCommand(light);
         Command setThermostat = new SetThermostatCommand(thermostat, 22);
+        Command lockDoor = new LockDoorCommand(door);
+
+        GoodnightModeCommand goodnightMode = new GoodnightModeCommand(Arrays.asList(turnOffLight, setThermostat, lockDoor));
 
         SmartHomeRemoteControl remote = new SmartHomeRemoteControl();
-        remote.assignCommandToSlot("light", turnOnLight);
-        remote.assignCommandToSlot("thermostat", setThermostat);
+        remote.addCommand("goodnight", goodnightMode);
+        remote.addCommand("turnOnLight", turnOnLight);
+        remote.addCommand("turnOffLight", turnOffLight);
+        remote.addCommand("setThermostat", setThermostat);
+        remote.addCommand("lockDoor", lockDoor);
 
-        System.out.println("--- Execute Commands ---");
-        remote.pressButton("light");
-        remote.pressButton("thermostat");
+        // Execute commands
+        System.out.println("Executing Goodnight Mode...");
+        remote.pressButton("goodnight");
 
-        System.out.println("\n--- Undo Last Command ---");
+        // Undo last command
+        System.out.println("\nUndo last command");
         remote.undoButton();
 
-        System.out.println("\n--- Execute Commands Again ---");
-        remote.pressButton("thermostat");
+        // Redo last command
+        System.out.println("\nRedo last command");
+        remote.redoButton();
     }
 }
