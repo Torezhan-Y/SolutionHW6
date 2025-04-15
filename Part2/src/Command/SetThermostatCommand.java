81f1a6cfb49962456f3f987186f642305cc3ef64
Torezhan Yelamanov <yyelamanooovv@gmail.com>
2025-04-15 18:53:07 +0500

    Created SetThermostatCommand to dial the perfect temperature

--- Part2/src/Command/SetThermostatCommand.java
+++ Part2/src/Command/SetThermostatCommand.java
@@ -5,20 +5,22 @@ import Device.Thermostat;
 public class SetThermostatCommand implements Command {
     private Thermostat thermostat;
     private int temperature;
+    private int previousTemperature;
 
     public SetThermostatCommand(Thermostat thermostat, int temperature) {
         this.thermostat = thermostat;
         this.temperature = temperature;
     }
 
+
     @Override
     public void execute() {
+        previousTemperature = thermostat.getTemperature(); // Save previous temperature
         thermostat.setTemperature(temperature);
     }
 
     @Override
     public void undo() {
-        thermostat.revertToPreviousTemperature();
+        thermostat.revertTemperature(previousTemperature);
     }
 }
-
